d65f519c866112d544bc87fe4b49fa185de9aef7
Abhishek32971 <97088891+Abhishek32971@users.noreply.github.com>
2024-03-11 19:16:25 +0530

    Update budget.py

--- backend/budget.py
+++ backend/budget.py
@@ -1,19 +1,1 @@
-import time
-from pydantic import BaseModel
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
-class TokenData(BaseModel):
-    source:str
-    destination:str
-    
 
-app = FastAPI()
-
-
-
-    # finally:
-    #     driver.quit()
